88d144fc90c4a7f305c264d3cf0eb3db0974aa4c
Eric Torunski <etorunski@gmail.com>
2020-11-03 10:23:12 -0500

    week 6 AsyncTask http example

--- app/src/main/java/com/example/inclassexamples_f20/MainActivity.java
+++ app/src/main/java/com/example/inclassexamples_f20/MainActivity.java
@@ -1,10 +1,16 @@
 package com.example.inclassexamples_f20;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,12 +22,31 @@ public class MainActivity extends AppCompatActivity {
         MyHTTPRequest req = new MyHTTPRequest();
         req.execute("http://torunski.ca/CST2335_XML.xml");  //Type 1
     }
+
+
     //Type1     Type2   Type3
     private class MyHTTPRequest extends AsyncTask< String, Integer, String>
     {
         //Type3                Type1
         public String doInBackground(String ... args)
         {
+            try {
+
+                //create a URL object of what server to contact:
+                URL url = new URL(args[0]);
+
+                //open the connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                //wait for data:
+                InputStream response = urlConnection.getInputStream();
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
             return "Done";
         }
 
