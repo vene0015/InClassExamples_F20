c0c3f34155f981983a254f9617b434ff415618fb
Eric Torunski <etorunski@gmail.com>
2020-11-03 10:20:35 -0500

    week 6 AsyncTask example

--- app/src/main/java/com/example/inclassexamples_f20/MainActivity.java
+++ app/src/main/java/com/example/inclassexamples_f20/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.inclassexamples_f20;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override   //like main function
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);  //loads stuff to the screen
+        setContentView(R.layout.activity_main);
+
+        MyHTTPRequest req = new MyHTTPRequest();
+        req.execute("http://torunski.ca/CST2335_XML.xml");  //Type 1
+    }
+    //Type1     Type2   Type3
+    private class MyHTTPRequest extends AsyncTask< String, Integer, String>
+    {
+        //Type3                Type1
+        public String doInBackground(String ... args)
+        {
+            return "Done";
+        }
+
+        //Type 2
+        public void onProgressUpdate(Integer ... args)
+        {
+
+        }
+        //Type3
+        public void onPostExecute(String fromDoInBackground)
+        {
+            Log.i("HTTP", fromDoInBackground);
+        }
     }
 }
