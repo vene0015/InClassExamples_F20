86ce308d78bf27f6cbb371e1675104619cdfef3c
Eric Torunski <torunse@algonquincollege.com>
2020-11-03 19:10:45 -0500

    finished slide 19

--- app/src/main/java/com/example/inclassexamples_f20/MainActivity.java
+++ app/src/main/java/com/example/inclassexamples_f20/MainActivity.java
@@ -11,6 +11,7 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,20 +21,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         MyHTTPRequest req = new MyHTTPRequest();
-        req.execute("http://torunski.ca/CST2335_XML.xml");  //Type 1
+        req.execute("https://torunski.ca/","CST2335_XML.xml");  //Type 1
     }
 
 
-    //Type1     Type2   Type3
+                                                 //Type1     Type2   Type3
     private class MyHTTPRequest extends AsyncTask< String, Integer, String>
     {
-        //Type3                Type1
+        //Type3                      Type1
         public String doInBackground(String ... args)
         {
             try {
+                String encoded = args[0] + URLEncoder.encode(args[1], "UTF-8");
 
                 //create a URL object of what server to contact:
-                URL url = new URL(args[0]);
+                URL url = new URL(encoded);
 
                 //open the connection
                 HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
