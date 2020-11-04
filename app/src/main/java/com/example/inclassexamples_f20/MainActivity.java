584c615b49427af923070db414178be1cd1a8656
Eric Torunski <etorunski@gmail.com>
2020-11-03 19:13:07 -0500

    week 6 AsyncTask xml example

--- app/src/main/java/com/example/inclassexamples_f20/MainActivity.java
+++ app/src/main/java/com/example/inclassexamples_f20/MainActivity.java
@@ -6,6 +6,13 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
@@ -16,12 +23,76 @@ public class MainActivity extends AppCompatActivity {
         MyHTTPRequest req = new MyHTTPRequest();
         req.execute("http://torunski.ca/CST2335_XML.xml");  //Type 1
     }
-    //Type1     Type2   Type3
+                                                //Type1     Type2   Type3
     private class MyHTTPRequest extends AsyncTask< String, Integer, String>
     {
-        //Type3                Type1
+        //Type3                     Type1
         public String doInBackground(String ... args)
         {
+            try {
+
+                //create a URL object of what server to contact:
+                URL url = new URL(args[0]);
+
+                //open the connection
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+
+                //wait for data:
+                InputStream response = urlConnection.getInputStream();
+
+
+
+                //From part 3: slide 19
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                factory.setNamespaceAware(false);
+                XmlPullParser xpp = factory.newPullParser();
+                xpp.setInput( response  , "UTF-8");
+
+
+
+                //From part 3, slide 20
+                String parameter = null;
+
+                int eventType = xpp.getEventType(); //The parser is currently at START_DOCUMENT
+
+                while(eventType != XmlPullParser.END_DOCUMENT)
+                {
+
+                    if(eventType == XmlPullParser.START_TAG)
+                    {
+                        //If you get here, then you are pointing at a start tag
+                        if(xpp.getName().equals("Weather"))
+                        {
+                            //If you get here, then you are pointing to a <Weather> start tag
+                            String outlook = xpp.getAttributeValue(null,    "outlook");
+                            String windy = xpp.getAttributeValue(null, "windy");
+                        }
+
+                        else if(xpp.getName().equals("AMessage"))
+                        {
+                            parameter = xpp.getAttributeValue(null, "message"); // this will run for <AMessage message="parameter" >
+                        }
+                        else if(xpp.getName().equals("Weather"))
+                        {
+                            parameter = xpp.getAttributeValue(null, "outlook"); //this will run for <Weather outlook="parameter"
+                            parameter = xpp.getAttributeValue(null, "windy"); //this will run for <Weather windy="paramter"  >
+                        }
+                        else if(xpp.getName().equals("Temperature"))
+                        {
+                            xpp.next(); //move the pointer from the opening tag to the TEXT event
+                            parameter = xpp.getText(); // this will return  20
+                        }
+                    }
+                    eventType = xpp.next(); //move to the next xml event and store it in a variable
+                }
+
+
+            }
+            catch (Exception e)
+            {
+
+            }
+
             return "Done";
         }
 
